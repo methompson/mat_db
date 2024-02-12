796a04b11bd82b8aaaca2e231a6359a4155c9fa8
Mat Thompson <met@methompson.com>
2024-02-12 09:37:23 -0600

    revised addData to add indexes

--- src/db/data.ts
+++ src/db/data.ts
@@ -1,6 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
+import { addIndexedSortKey } from './indexed_sort_keys';
+import { addSortedKey } from './sorted_keys';
+
 /**
  * The data.
  *
  * The data is stored as stringified JSON. The keys are the IDs of the data.
  */
-export const data: Record<string, string> = {};
+export const theData: Record<string, string> = {};
+
+export function addData(data: string, id?: string | undefined): void {
+  const setId = id ?? uuidv4();
+
+  theData[setId] = data;
+
+  // TODO determine which data constructs sort keys
+  const mysortKey = 'sortKey';
+
+  addIndexedSortKey(setId, mysortKey);
+  addSortedKey(setId, mysortKey);
+  addIndexedKeyPermutations(setId, mysortKey);
+}
